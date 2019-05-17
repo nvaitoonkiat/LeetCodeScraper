5df15ee42784354a3e275a88939cf6d97cec6b75
nvaitoonkiat <nvaitoonkiat@gmail.com>
2019-05-17 18:26:00 -0400

    Saved Q.urls in array and grabbed question

--- question.py
+++ question.py
@@ -1,16 +1,14 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 import requests, sys, json, time, lxml
 
 # Grab the algorithms page of LeetCode and wait (2.5 s) for problems 
 # to dynamically load using selenium
-urlMain = "https://leetcode.com/problemset/algorithms/"
+urlMain = "https://leetcode.com/problems/two-sum/"
 browser = webdriver.Chrome()
-browser.implicitly_wait(30)
 browser.get(urlMain)
-browser.find_element_by_class_name("question-list-table")
-# //*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[2]/tr/td/span/select
+time.sleep(2.5)
+
 # Use BeautifulSoup4 to extract HTML source code for 
 # links to each question and intialize array to store them
 soup = BeautifulSoup(browser.page_source, 'lxml')
@@ -24,13 +22,15 @@ for q in questionsList:
 
 for urlQuestion in questionLinks:
     # print(urlQuestion)
-    browser.implicitly_wait(30)
-    browser.get("https://leetcode.com/" + urlQuestion)
-    browser.find_element_by_class_name("content__u3I1")
+    browser2 = webdriver.Chrome()
+    browser2.get("https://leetcode.com/" + urlQuestion)
+    time.sleep(5)
     soup_level2 = BeautifulSoup(browser.page_source, 'lxml')
     question = soup_level2.find_all('div', class_='content__u3I1')
-    for qst in question: 
-        print(qst.text)
+    print(question)
+    # print(question.text)
+    browser2.close()
+    
 
 
 # with open('scrape.txt', 'w', encoding='utf-8') as file:
