a448142b1ea28edc69042db265ca17abde78ef2c
nvaitoonkiat <nvaitoonkiat@gmail.com>
2019-05-20 09:44:44 -0400

    Added click to dropdown
    selects all questions button on dropdown

--- leetcodeScraper_final.py
+++ leetcodeScraper_final.py
@@ -10,15 +10,21 @@ browser = webdriver.Chrome()
 browser.implicitly_wait(30)
 browser.get(urlMain)
 browser.find_element_by_class_name("question-list-table")
-# //*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[2]/tr/td/span/select
+browser.find_element_by_xpath("//*[@id='question-app']/div/div[2]/div[2]/div[2]/table/tbody[2]/tr/td/span/select/option[@value='9007199254740991']").click()
+# 
 # Use BeautifulSoup4 to extract HTML source code for 
 # links to each question and intialize array to store them
 soup = BeautifulSoup(browser.page_source, 'lxml')
 questionsList = soup.find_all('tbody', class_='reactable-data')
 questionLinks = []
 
+# with open('scrape.txt', 'w', encoding='utf-8') as file:
+#     # file.write(text)
+#     # file.write(element.text)
+#     file.write(soup.prettify())
+
 for q in questionsList:
-    for a in q.find_all('a', href = lambda href: href and "/problem" in href):
+    for a in q.find_all('a', href=lambda href: href and "/problem" in href):
         # print(a['href'])
         questionLinks.append(a['href'])
 
@@ -29,13 +35,10 @@ for urlQuestion in questionLinks:
     browser.find_element_by_class_name("content__u3I1")
     soup_level2 = BeautifulSoup(browser.page_source, 'lxml')
     question = soup_level2.find_all('div', class_='content__u3I1')
-    for qst in question: 
+    for qst in question:
         print(qst.text)
 
 
-# with open('scrape.txt', 'w', encoding='utf-8') as file:
-#     # file.write(text)
-#     # file.write(element.text)
-#     file.write(str(test))
+
 
 browser.quit()
